08fd2333e04e3912c84030a9c24e15aa1fc91bba
TharshanSM <Ugatharshan95@gmail.com>
2022-07-25 09:59:02 +0530

    Initial Commit

---
+++ routes/shop.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+const express = require("express");
+
+const rootDir = require("../util/path");
+const { products } = require("./admin");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  console.log(products);
+});
+
+module.exports = router;
